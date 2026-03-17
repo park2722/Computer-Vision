fab35c8fd5cf3219b6da4c2e57e58f9d90e297f4
Rusan <104426978+park2722@users.noreply.github.com>
2026-03-17 21:26:51 +0900

    Implement image capture feature in camera mode
    Added functionality to capture images while in camera mode.

--- Mode_Swiching_Camera.py
+++ Mode_Swiching_Camera.py
@@ -18,6 +18,7 @@ out = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
 
 recording = False  # Record 모드 여부
 gray_mode = False # Grayscale 모드 여부
+capture_count = 0  # 파일 이름용
 
 while True:
 
@@ -30,11 +31,20 @@ while True:
 
         # 빨간 원 표시
         cv2.circle(frame, (30, 30), 10, (0, 0, 255), -1)
-
         # 영상 파일에 저장
         out.write(frame)
 
-    elif gray_mode:
+    elif not recording:
+        # 녹화 중이 아닐 때는 "Camera" 텍스트 표시
+        cv2.putText(frame, "Camera", (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 0, 0), 4)   # 검은색 (두껍게)
+
+        cv2.putText(frame, "Camera", (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 255, 255), 2)  # 흰색 (얇게)
+
+    if gray_mode:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
     # 화면 출력
@@ -53,13 +63,24 @@ while True:
             print("Recording START")
         else:
             print("Recording STOP")
-
+    # C → 그레이스케일 모드 전환
     elif key ==ord('c'):
         gray_mode = not gray_mode
         if gray_mode:
             print("Grayscale Mode ON")
         else:
             print("Grayscale Mode OFF")
+    # S → 화면 캡처
+    elif key == ord('s'):
+        if recording:
+            print("Cannot capture while recording!")
+            continue
+        else:
+            filename = f"capture_{capture_count}.png"
+            cv2.imwrite(filename, frame)
+            print(f"{filename} saved")
+            capture_count += 1
+
 
 # 자원 해제
 cap.release()
